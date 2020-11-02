48b12873ff7c4384c8dd674ab058937243546b67
CinatitBR <cinatit.igor.santos@gmail.com>
2020-11-02 00:22:04 -0300

    Set the connection to MySQL database

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const mysql = require('mysql');
 
 const app = express();
 
 app.use(express.static('public'));
-app.set('view engine', 'njk')
+app.set('view engine', 'njk');
 
 nunjucks.configure('views', {
   autoescape: true,
   express: app
 });
 
+// Connecting to MySQL database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '12345',
+  database: 'menagerie'
+});
+
 app.get('/', (req, res) => {
-  return res.render('index', { name: 'Casarão' });
+  const sql = 'SELECT * FROM `pet`';
+
+  connection.query(sql, (error, results) => {
+    if (error) throw error;
+
+    // res.send(results[0].name)
+    return res.render('index', { results });
+  });
+
+  return;
 });
 
 const PORT = process.env.PORT || 3000;
